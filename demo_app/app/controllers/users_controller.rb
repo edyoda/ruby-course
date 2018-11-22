0bada9a7e30e6eb1c6f657d600776a60302990d4
Manvendra <manvendra_tripathi@apple.com>
2018-11-22 16:03:14 +0530

    added comments to the users controller

--- demo_app/app/controllers/users_controller.rb
+++ demo_app/app/controllers/users_controller.rb
@@ -3,9 +3,11 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+#GET /users/new 
   def new
     @user = User.new
   end
+#POST /userrs # create method takes all the values from the new.html.erb and save into DB
   def create
     @user = User.new(user_params)
     if @user.save
@@ -14,9 +16,11 @@ class UsersController < ApplicationController
       render "new"
     end
   end
+#GET /users/:id/edit
   def edit
     @user = User.find(params[:id])
   end
+#PATCH /users/:id # update method takes all the values from the edit.html.erb and save into DB
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -25,11 +29,14 @@ class UsersController < ApplicationController
       render "new"
     end
   end
+  # GET /users/:id display the data for every individual
   def show
     @user = User.find(params[:id])
   end
   private
   def user_params
+  # params contains meta data along with actual data so below logic will return the actual data which will get use 
+  # by the create or update actions
     params.require(:user).permit!
   end
 end
