3adecafa73b432c24defc109a71aeae20c0c7511
Manvendra <manvendra_tripathi@apple.com>
2018-11-22 15:54:18 +0530

    completed rails new, create,show and edit

--- demo_app/app/controllers/users_controller.rb
+++ demo_app/app/controllers/users_controller.rb
@@ -1,13 +1,35 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
-
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to edit_user_path(@user.id)
+    else
+      render "new"
+    end
+  end
   def edit
+    @user = User.find(params[:id])
   end
-
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render "new"
+    end
+  end
   def show
+    @user = User.find(params[:id])
+  end
+  private
+  def user_params
+    params.require(:user).permit!
   end
 end
--- demo_app/config/routes.rb
+++ demo_app/config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
 
-  get 'users/new'
-
-  get 'users/edit'
-
-  get 'users/show'
-
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
